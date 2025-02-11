20ec43b418de424e3c701a978eb683b611158aec
Lévy <prakkmak@gmail.com>
2025-02-11 22:04:45 +0100

    :white_check_mark: Add validation constraints to DogDto

--- src/main/java/com/dtos/DogDto.java
+++ src/main/java/com/dtos/DogDto.java
@@ -1,12 +1,19 @@
 package com.dtos;
 
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class DogDto {
 	
-	private Long Id;
+	@NotNull
+	private Long id;
+	
+	@NotBlank(message = "Le nom est obligatoire")
 	private String name;
+	
+	@NotBlank(message = "La race est obligatoire")
 	private String race;
 	
 }
