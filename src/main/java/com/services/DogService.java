2b5f7f2cc68f417104494f473415cd75f5cee52c
Lévy <prakkmak@gmail.com>
2025-02-11 21:55:06 +0100

    :memo: Update DogService interface with improved documentation

--- src/main/java/com/services/DogService.java
+++ src/main/java/com/services/DogService.java
@@ -4,26 +4,36 @@ import com.dtos.DogDto;
 
 import java.util.List;
 
+/**
+ * Interface définissant les opérations métier disponibles pour la gestion des chiens.
+ * Cette interface suit le principe d'Interface Segregation (SOLID).
+ */
 public interface DogService {
     /**
-     * Sauve a dog
+     * Sauvegarde un chien dans le système
+     * @param dogDto les données du chien à sauvegarder
+     * @return le chien sauvegardé avec son ID généré
      */
     DogDto saveDog(DogDto dogDto);
 
     /**
-     * Get a dog by it's id
+     * Récupère un chien par son identifiant
+     * @param dogId l'identifiant du chien recherché
+     * @return le chien trouvé
+     * @throws jakarta.persistence.EntityNotFoundException si le chien n'existe pas
      */
     DogDto getDogById(Long dogId);
 
     /**
-     * Delete a dog by it's id
+     * Supprime un chien du système
+     * @param dogId l'identifiant du chien à supprimer
+     * @return true si la suppression a réussi
      */
     boolean deleteDog(Long dogId);
 
     /**
-     * Get all the dogs
+     * Récupère tous les chiens du système
+     * @return la liste des chiens
      */
     List<DogDto> getAllDogs();
-
-
 }
